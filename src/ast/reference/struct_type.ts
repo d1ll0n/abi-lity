f7a08a7ed9f3a6c07955615eaa897e664411b632
d1ll0n <dillon@indexed.finance>
2022-11-23 07:37:59 -0600

    Add pp, copy, writeDefinition

--- src/ast/reference/struct_type.ts
+++ src/ast/reference/struct_type.ts
@@ -1,116 +1,40 @@
 import { ABITypeKind } from "../../constants";
 import { TypeNode } from "../type_node";
-import { ReferenceType } from "./reference_type";
+import { TupleLikeType } from "./tuple_type";
 
-export class StructType extends ReferenceType {
+export class StructType extends TupleLikeType {
   readonly kind = ABITypeKind.Struct;
   name: string;
 
-  members: TypeNode[] = [];
-  memberNames: string[] = [];
-
-  constructor(members: TypeNode[], memberNames: string[], name: string) {
-    super();
+  constructor(members: TypeNode[], name: string) {
+    super(members);
     this.name = name;
-    if (members.length !== memberNames.length) {
-      throw Error(`Struct must have same number of members and names`);
-    }
-    for (let i = 0; i < memberNames.length; i++) {
-      this.memberNames.push(memberNames[i]);
-      this.members.push(members[i]);
-    }
-    this.acceptChildren();
   }
 
   copy(): StructType {
-    return new StructType(
-      this.members.map((member) => member.copy()),
-      [...this.memberNames],
+    const struct = new StructType(
+      this.vMembers.map((member) => member.copy()),
       this.name
     );
-  }
-
-  get children(): TypeNode[] {
-    return this.pickNodes(this.members);
+    struct.labelFromParent = this.labelFromParent;
+    return struct;
   }
 
   get canonicalName(): string {
     return this.name;
   }
 
-  get isDynamicallyEncoded(): boolean {
-    return this.members.some((m) => m.isDynamicallyEncoded);
-  }
-
-  get calldataEncodedSize(): number {
-    if (this.isDynamicallyEncoded) {
-      throw Error(`Can not read calldata size of dynamically encoded struct`);
-    }
-    return this.members.reduce((sum, member) => sum + member.calldataEncodedSize, 0);
-  }
-
-  get calldataEncodedTailSize(): number {
-    if (!this.isDynamicallyEncoded) {
-      throw Error(`Can not read calldata tail of statically encoded struct`);
-    }
-    return this.members.reduce((sum, member) => sum + member.calldataHeadSize, 0);
-  }
-
-  get memoryDataSize(): number | undefined {
-    let sum = 0;
-    for (const member of this.members) {
-      const size = member.memoryDataSize;
-      if (size === undefined) return undefined;
-      sum += size;
-    }
-    return sum;
-  }
-
-  get extendedMemoryDataSize(): number | undefined {
-    let sum = 0;
-    for (const member of this.members) {
-      const size = member.extendedMemoryDataSize;
-      if (size === undefined) return undefined;
-      sum += size;
-    }
-    return sum;
-  }
-
-  signatureInExternalFunction(_structsByName: boolean): string {
-    if (_structsByName) return this.canonicalName;
-    const memberTypeStrings = this.children.map((c) =>
-      c.signatureInExternalFunction(_structsByName)
+  pp(): string {
+    const memberTypeStrings = this.children.map(
+      (c, i) => `  ` + [c.canonicalName, c.labelFromParent].filter(Boolean).join(" ") + ";"
     );
-    return "(" + memberTypeStrings.join(",") + ")";
+    return [`struct ${this.name} {`, ...memberTypeStrings, "}"].join("\n");
   }
 
-  calldataOffsetOfMember(name: string): number {
-    let offset = 0;
-    for (let i = 0; i < this.memberNames.length; i++) {
-      const member = this.members[i];
-      if (this.memberNames[i] === name) {
-        return offset;
-      } else {
-        offset += member.calldataHeadSize;
-      }
-    }
-    throw Error(`Member ${name} not found in struct`);
+  writeDefinition(): string {
+    const memberTypeStrings = this.children.map(
+      (c, i) => `  ` + [c.canonicalName, c.labelFromParent].filter(Boolean).join(" ") + ";"
+    );
+    return [`struct ${this.name} {`, ...memberTypeStrings, "}"].join("\n");
   }
-
-  memoryOffsetOfMember(name: string): number {
-    let offset = 0;
-    for (let i = 0; i < this.memberNames.length; i++) {
-      const member = this.members[i];
-      if (this.memberNames[i] === name) {
-        return offset;
-      } else {
-        offset += member.memoryHeadSize;
-      }
-    }
-    throw Error(`Member ${name} not found in struct`);
-  }
-
-  encodingType = undefined;
-  unpaddedSize = undefined;
-  isDynamicallySized = false;
 }
