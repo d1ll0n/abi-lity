f49d4402677c3d59b0c58768f6f24ffd1f457cd3
d1ll0n <dillon@indexed.finance>
2022-11-23 07:40:33 -0600

    Add union types

--- src/ast/reference/index.ts
+++ src/ast/reference/index.ts
@@ -1,5 +1,12 @@
+import { ArrayType } from "./array_type";
+import { BytesType, StringType } from "./packed_array_type";
+import { StructType } from "./struct_type";
+import { TupleType } from "./tuple_type";
+
 export * from "./array_type";
 export * from "./packed_array_type";
 export * from "./struct_type";
 export * from "./tuple_type";
 export * from "./reference_type";
+
+export type UReferenceType = ArrayType | BytesType | StringType | StructType | TupleType;
--- src/ast/value/index.ts
+++ src/ast/value/index.ts
@@ -1,9 +1,17 @@
+import { AddressType } from "./address_type";
+import { BoolType } from "./bool_type";
+import { EnumType } from "./enum_type";
+import { FixedBytesType } from "./fixed_bytes_type";
+import { IntegerType } from "./integer_type";
+
 export * from "./address_type";
 export * from "./bool_type";
 export * from "./enum_type";
-export * from "./fixed_bytes";
+export * from "./fixed_bytes_type";
 export * from "./integer_type";
 export * from "./value_type";
 export * from "./function_type";
 export * from "./error_type";
 export * from "./event_type";
+
+export type UValueType = AddressType | BoolType | FixedBytesType | IntegerType | EnumType;
