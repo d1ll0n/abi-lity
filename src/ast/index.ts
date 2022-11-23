54516ba4a24fbd9a1f014d93aaa3a8f92df9d47c
d1ll0n <dillon@indexed.finance>
2022-11-23 07:38:59 -0600

    Add union type

--- src/ast/index.ts
+++ src/ast/index.ts
@@ -1,4 +1,10 @@
+import { UReferenceType } from "./reference";
+import { UValueType } from "./value";
+
 export * from "./reference";
 export * from "./value";
 export * from "./type_node";
 export * from "./type_provider";
+export * from "./ast_context";
+
+export type UABIType = UReferenceType | UValueType;
