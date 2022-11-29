26dd950c31e2bdf6e7a920e16749d6e5e5a79617
d1ll0n <dillon@indexed.finance>
2022-11-29 15:52:46 -0600

    Add constants and index

---
+++ src/constants.ts
@@ -0,0 +1,23 @@
+export enum ArrayKind {
+  Ordinary = "ordinary",
+  Bytes = "bytes",
+  String = "string"
+}
+
+export enum ABITypeKind {
+  Bool = "bool",
+  Byte = "byte",
+  Uint = "uint",
+  Int = "int",
+  Address = "address",
+  Enum = "enum",
+  Array = "array",
+  FixedBytes = "fixed_bytes",
+  Bytes = "bytes",
+  String = "string",
+  Tuple = "tuple",
+  Struct = "struct",
+  Function = "function",
+  Error = "error",
+  Event = "event"
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./ast";
+export * from "./utils";
+export * from "./constants";
+export * from "./codegen";
+export * from "./readers";
