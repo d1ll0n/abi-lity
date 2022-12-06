69602fc45e2e2dded05f9247048a9d58bff7c632
d1ll0n <dillon@indexed.finance>
2022-12-06 01:16:14 -0600

    rm seaport example temporarily

--- examples/seaport/conduit/lib/ConduitEnums.sol
+++
@@ -1,3 +0,0 @@
-pragma solidity ^0.8.7;
-
-enum ConduitItemType { NATIVE, ERC20, ERC721, ERC1155 }
--- examples/seaport/interfaces/EIP1271Interface.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.8.7;
-
-interface EIP1271Interface {
-  function isValidSignature(bytes32 digest, bytes calldata signature) external view returns (bytes4);
-}
