fd3030ae4f3810f17fa120e93a671ed1446f9ebc
d1ll0n <dillon@indexed.finance>
2023-07-01 14:15:39 -0500

    use new coders

--- src/codegen/generate.ts
+++ src/codegen/generate.ts
@@ -3,22 +3,19 @@ import {
   ContractDefinition,
   ContractKind,
   Mapping,
-  SourceUnit,
   staticNodeFactory,
   StructDefinition
 } from "solc-typed-ast";
 import { structDefinitionToTypeNode } from "../readers";
 import { addImports, coerceArray, CompileHelper, Logger, NoopLogger } from "../utils";
-import {
-  buildDecoderFile,
-  buildExternalWrapper,
-  replaceExternalFunctionReferenceTypeParameters
-} from "./abi_decode";
+
 import { getForgeJsonSerializeFunction } from "./serialize/forge_json";
-import { getFunctionSelectorSwitch } from "./function_switch";
 import { CodegenContext } from "./utils";
 import { getForgeAssertEqualityFunction } from "./serialize/forge_assert";
-import { getSTDAssertionsShim, getVMShim } from "./solidity_libraries";
+import { getLibJson, getSTDAssertionsShim } from "./solidity_libraries";
+
+export { addExternalWrappers, upgradeSourceCoders } from "./coders";
+export { generateJsonSerializers } from "./serialize/generate";
 
 type CoderOptions = {
   functionSwitch: boolean;
@@ -30,50 +27,6 @@ const defaultOptions: CoderOptions = {
   functionSwitch: true
 };
 
-export function upgradeSourceCoders(
-  helper: CompileHelper,
-  fileName: string,
-  options: CoderOptions = defaultOptions,
-  logger: Logger = new NoopLogger()
-): void {
-  const decoderFileName = options.decoderFileName ?? fileName.replace(".sol", "Decoder.sol");
-  let decoderSourceUnit: SourceUnit;
-  if (helper.hasSourceUnit(decoderFileName)) {
-    decoderSourceUnit = helper.getSourceUnit(decoderFileName);
-  } else {
-    logger.log(`generating decoders for ${fileName}...`);
-    const ctx = buildDecoderFile(helper, fileName, decoderFileName, options.functionSwitch);
-    decoderSourceUnit = ctx.decoderSourceUnit;
-  }
-  const sourceUnit = helper.getSourceUnit(fileName);
-  logger.log(`replacing parameter declarations in ${fileName}...`);
-  replaceExternalFunctionReferenceTypeParameters(sourceUnit, decoderSourceUnit);
-  const contractDefinition = sourceUnit.getChildrenByType(ContractDefinition)[0];
-
-  if (!contractDefinition) {
-    throw Error(`No contracts found in ${fileName}`);
-  }
-
-  if (options.functionSwitch) {
-    logger.log(`generating function switch for ${contractDefinition.name}...`);
-    getFunctionSelectorSwitch(contractDefinition, decoderSourceUnit);
-  }
-}
-
-export function addExternalWrappers(
-  helper: CompileHelper,
-  fileName: string,
-  logger: Logger = new NoopLogger()
-): void {
-  const decoderFileName = fileName.replace(".sol", "External.sol");
-
-  logger.log(`generating wrappers for ${fileName}...`);
-  const ctx = buildExternalWrapper(helper, fileName, decoderFileName);
-}
-/* 
-
-address constant VM_ADDRESS = address(uint160(uint256(keccak256("hevm cheat code"))));
-IVm constant vm = IVm(VM_ADDRESS); */
 export function generateSerializers(
   helper: CompileHelper,
   fileName: string,
@@ -84,9 +37,9 @@ export function generateSerializers(
   const serializerFileName = options.decoderFileName ?? fileName.replace(".sol", "Serializers.sol");
   const ctx = new CodegenContext(helper, serializerFileName);
   const sourceUnit = helper.getSourceUnit(fileName);
-  const vmName = options.outPath ? path.join(options.outPath, `Temp___Vm.sol`) : `Temp___Vm.sol`;
-  const vm = helper.addSourceUnit(vmName, getVMShim());
-  addImports(ctx.decoderSourceUnit, vm, []);
+  const vmName = options.outPath ? path.join(options.outPath, `LibJson.sol`) : `LibJson.sol`;
+  const libJson = helper.addSourceUnit(vmName, getLibJson());
+  addImports(ctx.decoderSourceUnit, libJson, []);
   addImports(ctx.decoderSourceUnit, sourceUnit, []);
   let structDefinitions = sourceUnit
     .getChildrenByType(StructDefinition)
