c01a2b3cb42cfc570da3eb3ce28abbfa07454ee1
d1ll0n <dillon@indexed.finance>
2024-03-22 11:18:06 -0700

    add direct call check

---
+++ src/analysis/direct-calls.ts
@@ -0,0 +1,48 @@
+import {
+  ASTSearch,
+  ASTWriter,
+  ContractDefinition,
+  ContractKind,
+  DefaultASTWriterMapping,
+  ExternalReferenceType,
+  InferType,
+  LatestCompilerVersion,
+  MemberAccess,
+  PrettyFormatter
+} from "solc-typed-ast";
+import { CompileHelper } from "../utils/compile_utils/compile_helper";
+import path from "path";
+import { getParentSourceUnit, getRelativePath } from "../utils";
+const infer = new InferType(LatestCompilerVersion);
+
+function trackDirectCallsInContract(contract: ContractDefinition, basePath: string) {
+  const search = ASTSearch.fromContract(contract);
+  const examples = search
+    .find("FunctionCall", { vFunctionCallType: ExternalReferenceType.UserDefined })
+    .filter((call) => {
+      const closestContract =
+        call.vReferencedDeclaration?.getClosestParentByType(ContractDefinition);
+      if (!closestContract || closestContract.kind === ContractKind.Library) {
+        return false;
+      }
+      return infer.isFunctionCallExternal(call);
+    });
+  const writer = new ASTWriter(
+    DefaultASTWriterMapping,
+    new PrettyFormatter(2),
+    LatestCompilerVersion
+  );
+
+  examples.forEach((example) => {
+    const sourceUnit = getParentSourceUnit(example);
+    console.log(`In: ${getRelativePath(basePath, sourceUnit.absolutePath)}`);
+    console.log(writer.write(example));
+  });
+}
+async function testLookup() {
+  const basePath = "/home/pc/wildcat/v1.2/src/market/";
+  const helper = await CompileHelper.fromFileSystem(`WildcatMarket.sol`, basePath);
+  const source = helper.getSourceUnit("WildcatMarket.sol");
+  trackDirectCallsInContract(source.vContracts[0], basePath);
+}
+testLookup();
